8672b32b1b3a4ed670de77094875a33890041eaf
Brian Carcich <briantcarcich@gmail.com>
2023-11-27 13:49:57 -0500

    Add comments, rearrange some things

--- src/tests/from_MagAOX/git_state_tests.cpp
+++ src/tests/from_MagAOX/git_state_tests.cpp
@@ -13,30 +13,41 @@
 #include "types/generated/git_state_generated.h"
 #include "types/flatbuffer_log.hpp"
 
-const std::string gsName{"gsName"};
-const std::string gsSha1{"gsSha1"};
+const std::string gsName{"gsName"};  // Dummy GIT repo name
+const std::string gsSha1{"gsSha1"};  // Dummy GIT SHA1 value string
 
+/** Class to encapsulate GIT State flatbuffer data using
+ ** => types/git_state.hpp
+ ** => types/generated/git_state_generated.h
+ **/
 class GSM
 {
 public:
-  //MagAOX::logger::git_state::messageT gsm;
-  int L;
-  std::string m_saveGsName;
+  std::string m_saveGsName;    // Input parameters, unused
   std::string m_saveGsSha1;
   bool m_saveModified;
-  std::string m_gsName;
+
+  std::string m_gsName;        // Parameters retrieved from flatbuffer
   std::string m_gsSha1;
   bool m_modified;
 
+  int L;                       // flatbuffer data length; unused
+
+  // Constructor
   GSM(std::string gsName_, std::string gsSha1_, bool modified_) {
+    // Save inputs
     m_saveGsName = gsName_;
     m_saveGsSha1 = gsSha1_;
     m_saveModified = modified_;
   
+    // Create the flatbuffer using the input parameters and...::messageT
     auto gsm = MagAOX::logger::git_state::messageT(gsName_, gsSha1_, modified_);
-    void* p= gsm.builder.GetBufferPointer();
+
+    // Get flatbuffer pointer and length
+    void* p = gsm.builder.GetBufferPointer();
     L = MagAOX::logger::git_state::length(gsm);
 
+    // Retrieve the flatbuffer parameters for Catch2 comparison below
     m_gsName = std::string(MagAOX::logger::git_state::repoName(p));
     m_gsSha1 = MagAOX::logger::GetGit_state_fb(p)->sha1()->c_str();
     m_modified = MagAOX::logger::GetGit_state_fb(p)->modified();
@@ -47,15 +58,17 @@ SCENARIO( "Create", "[a log item]")
 {
     GIVEN("a git_state log code")
     {
-        WHEN("with modified true")
+        WHEN("[modified] value is true")
         {
             bool modif{true};
+            // Create flatbuffer
             GSM gs(gsName, gsSha1, modif);
+            // Compare flatbuffer parameters
             REQUIRE(gs.m_gsName == gsName);
             REQUIRE(gs.m_gsSha1 == gsSha1);
             REQUIRE(gs.m_modified == modif);
         }
-        WHEN("with modified false")
+        WHEN("[modified] value is false")
         {
             bool modif{false};
             GSM gs(gsName, gsSha1, modif);
