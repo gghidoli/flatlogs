2446323f7ff6162732411bf0e41817918c07a676
Brian Carcich <briantcarcich@gmail.com>
2023-11-27 19:20:14 -0500

    Add flatbuffer Verifier result to tests

--- src/tests/from_MagAOX/git_state_tests.cpp
+++ src/tests/from_MagAOX/git_state_tests.cpp
@@ -23,34 +23,64 @@ const std::string gsSha1{"gsSha1"};  // Dummy GIT SHA1 value string
 class GSM
 {
 public:
-  std::string m_saveGsName;    // Input parameters, unused
+  std::string m_saveGsName;  // Saved input parameter value; unused
   std::string m_saveGsSha1;
   bool m_saveModified;
 
-  std::string m_gsName;        // Parameters retrieved from flatbuffer
-  std::string m_gsSha1;
+  std::string m_name;        // Parameters retrieved from flatbuffer
+  std::string m_sha1;
   bool m_modified;
 
-  int L;                       // flatbuffer data length; unused
+  bool m_verify{false};      // Flatbuffers verifier success/failure
+
+  int L;                     // flatbuffer data length; used in ::verify
 
   // Constructor
-  GSM(std::string gsName_, std::string gsSha1_, bool modified_) {
-    // Save inputs
-    m_saveGsName = gsName_;
-    m_saveGsSha1 = gsSha1_;
-    m_saveModified = modified_;
-  
-    // Create the flatbuffer using the input parameters and...::messageT
-    auto gsm = MagAOX::logger::git_state::messageT(gsName_, gsSha1_, modified_);
+  GSM(std::string name_, std::string sha1_, bool mod_) {
+    // Save input parameters
+    m_saveGsName = name_;
+    m_saveGsSha1 = sha1_;
+    m_saveModified = mod_;
 
-    // Get flatbuffer pointer and length
+    // Create the flatbuffer messageT using the input parameters
+    auto gsm = MagAOX::logger::git_state::messageT(name_, sha1_, mod_);
+
+    // Retrieve flatbuffer pointer and length
     void* p = gsm.builder.GetBufferPointer();
     L = MagAOX::logger::git_state::length(gsm);
 
     // Retrieve the flatbuffer parameters for Catch2 comparison below
-    m_gsName = std::string(MagAOX::logger::git_state::repoName(p));
-    m_gsSha1 = MagAOX::logger::GetGit_state_fb(p)->sha1()->c_str();
+    m_name = std::string(MagAOX::logger::git_state::repoName(p));
+    m_sha1 = MagAOX::logger::GetGit_state_fb(p)->sha1()->c_str();
     m_modified = MagAOX::logger::GetGit_state_fb(p)->modified();
+
+    // Run the git_state verifier
+    m_verify = verify(gsm);
+  }
+
+  // Run the flatbuffer Verifier for this log type
+  // - requires bufferPtrT (shared_ptr<char*>) to full log entry
+  //   comprising log header plus flatbuffer log message
+  bool verify(const typename MagAOX::logger::git_state::messageT& msg) {
+
+    // Timestamp, prioriy (use nominal value here)
+    flatlogs::timespecX tsx{0,0};
+    flatlogs::logPrioT prio{flatlogs::logPrio::LOG_DEFAULT};
+
+    // Create full log: log header(*); log message (+)
+    // * Log level (priority)
+    // * Event code (implicit in <MagAOX::logger::type>)
+    // * Timestamp
+    // * Message size (variable length; 1, 2, or 8 bytes)
+    // + Message (variable length)
+
+    // N.G. allocates space and writes pointer value to logBuffer
+    flatlogs::bufferPtrT logBuffer;
+    flatlogs::logHeader::createLog<MagAOX::logger::git_state>(logBuffer
+                                                             , tsx, msg
+                                                             , prio);
+    // Run full log through flatbuffer Verifier
+    return MagAOX::logger::git_state::verify(logBuffer,L);
   }
 };
 
@@ -64,17 +94,19 @@ SCENARIO( "Create", "[a log item]")
             // Create flatbuffer
             GSM gs(gsName, gsSha1, modif);
             // Compare flatbuffer parameters
-            REQUIRE(gs.m_gsName == gsName);
-            REQUIRE(gs.m_gsSha1 == gsSha1);
+            REQUIRE(gs.m_name == gsName);
+            REQUIRE(gs.m_sha1 == gsSha1);
             REQUIRE(gs.m_modified == modif);
+            REQUIRE(gs.m_verify);
         }
         WHEN("[modified] value is false")
         {
             bool modif{false};
             GSM gs(gsName, gsSha1, modif);
-            REQUIRE(gs.m_gsName == gsName);
-            REQUIRE(gs.m_gsSha1 == gsSha1);
+            REQUIRE(gs.m_name == gsName);
+            REQUIRE(gs.m_sha1 == gsSha1);
             REQUIRE(gs.m_modified == modif);
+            REQUIRE(gs.m_verify);
         }
     }
 }
